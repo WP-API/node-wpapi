2079ac939e740def6cc33d82eb8a918d69bc93f2
carldanley <carldanley@gmail.com>
2014-06-03 18:31:07 -0400

    taxonomies are now in place and support all of the same methods described in https://github.com/WP-API/WP-API/blob/master/docs/guides/getting-started.md.

--- libs/taxonomies.js
+++ libs/taxonomies.js
@@ -4,10 +4,43 @@ var util = require( 'util' );
 module.exports = taxonomies;
 util.inherits( taxonomies, wpQuery );
 
-function taxonomies() {
-	this._id = id;
+function taxonomies( options ) {
+	this._options = options || {};
+	this._id = null;
+	this._supportedMethods = [ 'head', 'get' ];
+	this._action = null;
+	this._actionId = null;
 }
 
-taxonomies.prototype.me = function() {
+taxonomies.prototype.id = function( id ) {
+	if( this._action === null ) {
+		this._id = id;
+		this._supportedMethods = [ 'head', 'get', 'put', 'post', 'patch', 'delete' ];
+	} else if( this._action === 'terms' ) {
+		this._actionId = id;
+		this._supportedMethods = [ 'head', 'get', 'put', 'post', 'patch', 'delete' ];
+	}
+
 	return this;
+};
+
+taxonomies.prototype.terms = function() {
+	this._action = 'terms';
+	this._supportedMethods = [ 'head', 'get', 'post' ];
+	return this;
+};
+
+taxonomies.prototype.generateRequestUri = function () {
+	var path = [ this._options.host, this._options.basePath, 'taxonomies' ];
+	if( this._id !== null ) {
+		path.push( this._id );
+	}
+	if( this._action !== null ) {
+		path.push( this._action );
+	}
+	if( this._actionId !== null ) {
+		path.push( this._actionId );
+	}
+
+	return path.join( '/' ).replace( '//', '/' );
 };
