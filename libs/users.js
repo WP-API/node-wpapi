500b0c8db1365ca6d199c896ac57aa00958591f7
carldanley <carldanley@gmail.com>
2014-06-03 18:25:15 -0400

    users now work for all API methods

--- libs/users.js
+++ libs/users.js
@@ -4,10 +4,36 @@ var util = require( 'util' );
 module.exports = users;
 util.inherits( users, wpQuery );
 
-function users() {
-	this._id = id;
+function users( options ) {
+	this._options = options || {};
+	this._id = null;
+	this._action = null;
+	this._supportedMethods = [ 'head', 'get', 'post' ];
 }
 
 users.prototype.me = function() {
+	this._action = 'me';
+	this._supportedMethods = [ 'head', 'get', 'put', 'post', 'patch', 'delete' ];
 	return this;
+};
+
+users.prototype.id = function( id ) {
+	if( this._action === null ) {
+		this._id = parseInt( id, 10 );
+		this._supportedMethods = [ 'head', 'get', 'post' ];
+	}
+
+	return this;
+};
+
+users.prototype.generateRequestUri = function () {
+	var path = [ this._options.host, this._options.basePath, 'users' ];
+	if( this._id !== null ) {
+		path.push( this._id );
+	}
+	if( this._action !== null ) {
+		path.push( this._action );
+	}
+
+	return path.join( '/' ).replace( '//', '/' );
 };
