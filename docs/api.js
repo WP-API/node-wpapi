61386224889ba9d06e1c6164bfa544e5980b9a56
K.Adam White <kadamwhite@gmail.com>
2014-06-24 19:22:13 -0400

    Update docs for v0.1.1

--- docs/api.js
+++ docs/api.js
@@ -1,13 +1,52 @@
 YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
+        "CollectionFilters",
         "PostsRequest",
         "TaxonomiesRequest",
         "UsersRequest",
         "WP",
         "WPRequest"
     ],
-    "modules": [],
-    "allModules": []
+    "modules": [
+        "CollectionFilters",
+        "PostsRequest",
+        "TaxonomiesRequest",
+        "UsersRequest",
+        "WP",
+        "WPRequest"
+    ],
+    "allModules": [
+        {
+            "displayName": "CollectionFilters",
+            "name": "CollectionFilters",
+            "description": "CollectionFilters' \"mixins\" object is intended to extend endpoint modules' prototypes"
+        },
+        {
+            "displayName": "PostsRequest",
+            "name": "PostsRequest",
+            "description": "PostsRequest extends WPRequest to handle the /posts API endpoint"
+        },
+        {
+            "displayName": "TaxonomiesRequest",
+            "name": "TaxonomiesRequest",
+            "description": "TaxonomiesRequest extends WPRequest to handle the /taxonomies API endpoint"
+        },
+        {
+            "displayName": "UsersRequest",
+            "name": "UsersRequest",
+            "description": "UsersRequest extends WPRequest to handle the /users API endpoint"
+        },
+        {
+            "displayName": "WP",
+            "name": "WP",
+            "description": "A WP REST API client for Node.js"
+        },
+        {
+            "displayName": "WPRequest",
+            "name": "WPRequest",
+            "description": "WPRequest is the base API request object constructor"
+        }
+    ]
 } };
 });
