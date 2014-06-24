eb894dc7c129f6cf60f5ea4df09be177586c56a5
K.Adam White <kadamwhite@gmail.com>
2014-06-24 13:04:58 -0400

    Order filters alphabetically and test collection._queryStr()

--- tests/lib/shared/filters.js
+++ tests/lib/shared/filters.js
@@ -1,3 +1,4 @@
+/*jshint -W106 */// Disable underscore_case warnings in this file b/c WP uses them
 const chai = require( 'chai' );
 const expect = chai.expect;
 // chai.use( require( 'sinon-chai' ) );
@@ -57,6 +58,42 @@ describe( 'collection().filters', function() {
 				postsPerPage: 7,
 				postStatus: 'draft'
 			});
+		});
+
+	});
+
+	describe( '_queryStr', function() {
+
+		it( 'properly parses taxonomy filters', function() {
+			request._taxonomyFilters = {
+				tag: [ 'clouds ', 'islands' ],
+				custom_tax: [ 7 ]
+			};
+			var query = request._queryStr();
+			// Filters should be in alpha order, to support caching requests
+			expect( query ).to
+				.equal( '?filter%5Bcustom_tax%5D=7&filter%5Btag%5D=clouds%2Bislands' );
+		});
+
+		it( 'properly parses regular filters', function() {
+			request._filters = {
+				post_status: 'publish', s: 'Some search string'
+			};
+			var query = request._queryStr();
+			expect( query ).to
+				.equal( '?filter%5Bpost_status%5D=publish&filter%5Bs%5D=Some%20search%20string' );
+		});
+
+		it( 'correctly merges taxonomy and regular filters & renders them in order', function() {
+			request._taxonomyFilters = {
+				cat: [ 7, 10 ]
+			};
+			request._filters = {
+				name: 'some-slug'
+			};
+			var query = request._queryStr();
+			// Filters should be in alpha order, to support caching requests
+			expect( query ).to.equal( '?filter%5Bcat%5D=7%2B10&filter%5Bname%5D=some-slug' );
 		});
 
 	});
