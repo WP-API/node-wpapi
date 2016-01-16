35b6892bb4451a14bcc33930df53a0719552f2d6
K. Adam White <kadamwhite@users.noreply.github.com>
2016-01-16 17:59:14 -0500

    Add a rudimentary integration test suite for posts/pagination
    Tests getting posts and stepping forward and back through pages.

--- tests/integration/posts.js
+++ tests/integration/posts.js
@@ -1,8 +1,61 @@
 'use strict';
-var expect = require( 'chai' ).expect;
+var chai = require( 'chai' );
+// Chai-as-promised and the `expect( prom ).to.eventually.equal( 'success' ) is
+// used to ensure that the assertions running within the promise chains are
+// actually run.
+chai.use( require( 'chai-as-promised' ) );
+var expect = chai.expect;
 
 var WP = require( '../../' );
 var WPRequest = require( '../../lib/shared/wp-request.js' );
+
+// Define some arrays to use ensuring the returned data is what we expect
+// it to be (e.g. an array of the titles from posts on the first page)
+var expectedResults = {
+	titles: {
+		page1: [
+			'Markup: HTML Tags and Formatting',
+			'Markup: Image Alignment',
+			'Markup: Text Alignment',
+			'Markup: Title With Special Characters',
+			'Markup: Title With Markup',
+			'Template: Featured Image (Vertical)',
+			'Template: Featured Image (Horizontal)',
+			'Template: More Tag',
+			'Template: Excerpt (Defined)',
+			'Template: Excerpt (Generated)'
+		],
+		page2: [
+			'Template: Paginated',
+			'Template: Sticky',
+			'Template: Comments',
+			'Template: Comments Disabled',
+			'Template: Pingbacks And Trackbacks',
+			'Media: Twitter Embeds',
+			'Post Format: Standard',
+			'Post Format: Gallery',
+			'Post Format: Gallery (Tiled)'
+		],
+		page4: [
+			'Post Format: Quote',
+			'Post Format: Chat',
+			'Antidisestablishmentarianism',
+			'',
+			'Edge Case: No Content',
+			'Edge Case: Many Categories',
+			'Edge Case: Many Tags',
+			'Edge Case: Nested And Mixed Lists'
+		]
+	}
+};
+
+// Inspecting the titles of the returned posts arrays is an easy way to
+// validate that the right page of results was returned
+function getTitles( posts ) {
+	return posts.map(function( post ) {
+		return post.title.rendered;
+	});
+}
 
 describe( 'integration: posts()', function() {
 	var wp;
@@ -14,87 +67,106 @@ describe( 'integration: posts()', function() {
 	});
 
 	it( 'can be used to retrieve a list of recent posts', function() {
-		return wp.posts().then(function( posts ) {
+		var prom = wp.posts().get().then(function( posts ) {
 			expect( posts ).to.be.an( 'array' );
 			expect( posts.length ).to.equal( 10 );
+			return 'success';
 		});
+		return expect( prom ).to.eventually.equal( 'success' );
 	});
 
 	it( 'fetches the 10 most recent posts by default', function() {
-		return wp.posts().then(function( posts ) {
-			var titles = posts.map(function( post ) {
-				return post.title.rendered;
-			});
-			expect( titles ).to.deep.equal([
-				'Markup: HTML Tags and Formatting',
-				'Markup: Image Alignment',
-				'Markup: Text Alignment',
-				'Markup: Title With Special Characters',
-				'Markup: Title With Markup',
-				'Template: Featured Image (Vertical)',
-				'Template: Featured Image (Horizontal)',
-				'Template: More Tag',
-				'Template: Excerpt (Defined)',
-				'Template: Excerpt (Generated)'
-			]);
+		var prom = wp.posts().get().then(function( posts ) {
+			expect( getTitles( posts ) ).to.deep.equal( expectedResults.titles.page1 );
+			return 'success';
 		});
+		return expect( prom ).to.eventually.equal( 'success' );
 	});
 
 	describe( 'paging properties', function() {
 
 		it( 'are exposed as _paging on the response array', function() {
-			return wp.posts().then(function( posts ) {
+			var prom = wp.posts().get().then(function( posts ) {
 				expect( posts ).to.have.property( '_paging' );
 				expect( posts._paging ).to.be.an( 'object' );
+				return 'success';
 			});
+			return expect( prom ).to.eventually.equal( 'success' );
 		});
 
 		it( 'include the total number of posts', function() {
-			return wp.posts().then(function( posts ) {
+			var prom = wp.posts().get().then(function( posts ) {
 				expect( posts._paging ).to.have.property( 'total' );
 				expect( posts._paging.total ).to.equal( '38' );
+				return 'success';
 			});
+			return expect( prom ).to.eventually.equal( 'success' );
 		});
 
 		it( 'include the total number of pages available', function() {
-			return wp.posts().then(function( posts ) {
+			var prom = wp.posts().get().then(function( posts ) {
 				expect( posts._paging ).to.have.property( 'totalPages' );
 				expect( posts._paging.totalPages ).to.equal( '4' );
+				return 'success';
 			});
+			return expect( prom ).to.eventually.equal( 'success' );
 		});
 
 		it( 'provides a bound WPRequest for the next page as .next', function() {
-			return wp.posts().then(function( posts ) {
+			var prom = wp.posts().get().then(function( posts ) {
 				expect( posts._paging ).to.have.property( 'next' );
 				expect( posts._paging.next ).to.be.an( 'object' );
 				expect( posts._paging.next ).to.be.an.instanceOf( WPRequest );
 				expect( posts._paging.next._options.endpoint ).to
 					.equal( 'http://wpapi.loc/wp-json/wp/v2/posts?page=2' );
-			});
-		});
-
-		it.skip( 'allows access to the next page of results via .next', function() {
-			return wp.posts().then(function( posts ) {
-				return posts._paging.next.then(function( posts ) {
-					expect( posts ).to.be.an( 'array' );
-					expect( posts.length ).to.equal( 10 );
-					var titles = posts.map(function( post ) {
-						return post.title.rendered;
-					});
-					expect( titles ).not.to.deep.equal([
-						'Markup: HTML Tags and Formatting',
-						'Markup: Image Alignment',
-						'Markup: Text Alignment',
-						'Markup: Title With Special Characters',
-						'Markup: Title With Markup',
-						'Template: Featured Image (Vertical)',
-						'Template: Featured Image (Horizontal)',
-						'Template: More Tag',
-						'Template: Excerpt (Defined)',
-						'Template: Excerpt (Generated)'
-					]);
+				// Get last page & ensure "next" no longer appears
+				return wp.posts().page( posts._paging.totalPages ).get().then(function( posts ) {
+					expect( posts._paging ).not.to.have.property( 'next' );
+					expect( getTitles( posts ) ).to.deep.equal( expectedResults.titles.page4 );
+					return 'success';
 				});
 			});
+			return expect( prom ).to.eventually.equal( 'success' );
+		});
+
+		it( 'allows access to the next page of results via .next', function() {
+			var prom = wp.posts().get().then(function( posts ) {
+				return posts._paging.next.get().then(function( posts ) {
+					expect( posts ).to.be.an( 'array' );
+					expect( posts.length ).to.equal( 9 );
+					expect( getTitles( posts ) ).to.deep.equal( expectedResults.titles.page2 );
+					return 'success';
+				});
+			});
+			return expect( prom ).to.eventually.equal( 'success' );
+		});
+
+		it( 'provides a bound WPRequest for the previous page as .prev', function() {
+			var prom = wp.posts().get().then(function( posts ) {
+				expect( posts._paging ).not.to.have.property( 'prev' );
+				return posts._paging.next.get().then(function( posts ) {
+					expect( posts._paging ).to.have.property( 'prev' );
+					expect( posts._paging.prev ).to.be.an( 'object' );
+					expect( posts._paging.prev ).to.be.an.instanceOf( WPRequest );
+					expect( posts._paging.prev._options.endpoint ).to
+						.equal( 'http://wpapi.loc/wp-json/wp/v2/posts?page=1' );
+					return 'success';
+				});
+			});
+			return expect( prom ).to.eventually.equal( 'success' );
+		});
+
+		it( 'allows access to the previous page of results via .prev', function() {
+			var prom = wp.posts().page( 2 ).get().then(function( posts ) {
+				expect( getTitles( posts ) ).to.deep.equal( expectedResults.titles.page2 );
+				return posts._paging.prev.get().then(function( posts ) {
+					expect( posts ).to.be.an( 'array' );
+					expect( posts.length ).to.equal( 10 );
+					expect( getTitles( posts ) ).to.deep.equal( expectedResults.titles.page1 );
+					return 'success';
+				});
+			});
+			return expect( prom ).to.eventually.equal( 'success' );
 		});
 
 	});
