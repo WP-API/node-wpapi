e9b78f76aaaa24f5b8dab7e55b3b4e235f97b61c
K. Adam White <kadamwhite@users.noreply.github.com>
2016-09-29 00:28:21 -0400

    Exclude 404 page from top nav

--- build/grunt/clean.js
+++ build/grunt/clean.js
@@ -4,7 +4,7 @@ module.exports = function( grunt ) {
 	grunt.config.set( 'clean', {
 		generated_api_docs: [ 'documentation/api-reference' ],
 		generated_pages: [ 'documentation/*.md' ],
-		generated_index: [ 'documentation/index.html' ],
+		generated_files: [ 'documentation/index.html', 'documentation/404.html' ],
 		generated_zip: [ 'documentation/*.zip' ],
 		leftover_pages_from_docs_branch: [
 			'./Gemfile.lock',
