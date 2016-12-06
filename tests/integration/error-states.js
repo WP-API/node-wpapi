7273a9399cd5a00e7a55a18379f88245ba79cb0a
K. Adam White <kadamwhite@users.noreply.github.com>
2016-12-05 23:35:06 -0500

    Only pass API errors through; return transport-level errors as-is
    Fixes #266

---
+++ tests/integration/error-states.js
@@ -0,0 +1,28 @@
+'use strict';
+var chai = require( 'chai' );
+// Variable to use as our "success token" in promise assertions
+var SUCCESS = 'success';
+// Chai-as-promised and the `expect( prom ).to.eventually.equal( SUCCESS ) is
+// used to ensure that the assertions running within the promise chains are
+// actually run.
+chai.use( require( 'chai-as-promised' ) );
+var expect = chai.expect;
+
+var WPAPI = require( '../../' );
+
+describe( 'error states:', function() {
+
+	it( 'invalid root endpoint causes a transport-level (superagent) 404 error', function() {
+		var wp = WPAPI.site( 'http://wpapi.loc/wrong-root-endpoint' );
+		var prom = wp.posts()
+			.get()
+			.catch(function( err ) {
+				expect( err ).to.be.an.instanceOf( Error );
+				expect( err ).to.have.property( 'status' );
+				expect( err.status ).to.equal( 404 );
+				return SUCCESS;
+			});
+		return expect( prom ).to.eventually.equal( SUCCESS );
+	});
+
+});
