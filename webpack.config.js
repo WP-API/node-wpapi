faaadd92e92bf738fda910551bc95aef310d1f38
K. Adam White <kadamwhite@users.noreply.github.com>
2018-12-20 16:37:47 -0500

    Upgrade Webpack to v4 and add babel-loader
    Babel-loader will bridge the gap between our Node 8-compatible syntax
    and modern web browsers (as well as less-modern ones like IE 11)

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,45 @@
 'use strict';
 
-const path = require( 'path' );
-const outputPath = path.join( __dirname, 'browser' );
+const { join } = require( 'path' );
 
 module.exports = {
 	entry: './wpapi.js',
 
-	devtool: 'source-map',
+	mode: 'development',
+
+	devtool: 'cheap-module-source-map',
+
+	stats: {
+		all: false,
+		assets: true,
+		colors: true,
+		errors: true,
+		performance: true,
+		timings: true,
+		warnings: true,
+	},
 
 	output: {
-		path: outputPath,
+		path: join( process.cwd(), 'browser' ),
 		filename: 'wpapi.js',
 		library: 'WPAPI',
 		libraryTarget: 'umd',
 	},
+
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules|bower_components)/,
+				loader: require.resolve( 'babel-loader' ),
+				options: {
+					presets: [ '@babel/preset-env' ],
+					plugins: [ '@babel/plugin-proposal-object-rest-spread' ],
+					// Cache compilation results in ./node_modules/.cache/babel-loader/
+					cacheDirectory: true,
+				},
+			},
+		],
+	},
+
 };
--- webpack.config.minified.js
+++ webpack.config.minified.js
@@ -1,15 +1,21 @@
 'use strict';
 
-const webpack = require( 'webpack' );
 const config = require( './webpack.config' );
 
 // Re-use normal Webpack build config, just adding minification
-config.output.filename = 'wpapi.min.js';
-config.plugins = config.plugins || [];
-config.plugins.push( new webpack.optimize.UglifyJsPlugin( {
-	compress: {
-		warnings: false,
-	},
-} ) );
+module.exports = {
+	...config,
 
-module.exports = config;
+	mode: 'production',
+
+	devtool: 'hidden-source-map',
+
+	output: {
+		...config.output,
+		filename: 'wpapi.min.js',
+	},
+
+	optimization: {
+		noEmitOnErrors: true,
+	},
+};
