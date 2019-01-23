8ea250ccd76311a69007e7e1e9229a9fa3968bff
K Adam White <kadamwhite@users.noreply.github.com>
2019-01-23 10:15:32 -0500

    Breaking: Simplify Auto-Discovery process (#413)
    Fixes #334; Fixes #355
    
    Headers and links are fairly brittle, and appending `?rest_route=/` to
    any page on a WordPress site (including `/wp-json/` and children) will
    return the root API index object. That response skips most of the song
    and dance involved in auto-discovery.
    
    Changes:
    
    - Directly request the API index using ?rest_route=/
    - Do not log any console warnings or errors during discovery, and remove
      unneeded stubs from test suite
    - Do not default to a configured WPAPI object if discovery fails
    - Directly return error object if discovery fails
    - Add integration tests to validate various types of discovery input URI
    
    Before:
    
    - Make a HEAD request to the provided uri
    - Try to read an endpoint URL out of HEAD request's response headers
      - If no response header is found, retry uri request as GET
      - Try to read an endpoint URL out of GET request's response headers
    - If an API endpoint is found, make a GET request to it
    - Try to bind to the returned API index object's routes list
    - If the API index request or binding failed, return a WPAPI instance
      configured to use the originally-provided endpoint uri, and log a
      console warning
    
    After:
    
    - Make a GET request to the provided uri, appending the rest_route=/
      query parameter to directly request the REST API index for the site
    - If the API index request succeeded, return a WPAPI instance configured
      to use the routes and endpoint discovered from the index
    - If the API index request or WPAPI instantiation failed, throw an error
    
    props @rmccue, @luisherranz, @qgustavor

--- wpapi.js
+++ wpapi.js
@@ -30,9 +30,6 @@ let defaultEndpointFactories;
 // Constant used to detect first-party WordPress REST API routes
 const apiDefaultNamespace = 'wp/v2';
 
-// Pull in autodiscovery methods
-const autodiscovery = require( './lib/autodiscovery' );
-
 // Pull in base module constructors
 const WPRequest = require( './lib/constructors/wp-request' );
 
@@ -410,46 +407,17 @@ WPAPI.prototype.namespace = function( namespace ) {
  * library is unable to parse the provided endpoint.
  */
 WPAPI.discover = ( url ) => {
-	// local placeholder for API root URL
-	let endpoint;
-
-	// Try HEAD request first, for smaller payload: use WPAPI.site to produce
-	// a request that utilizes the defined HTTP transports
-	const req = WPAPI.site( url ).root();
-	return req.headers()
-		.catch( () => {
-			// On the hypothesis that any error here is related to the HEAD request
-			// failing, provisionally try again using GET because that method is
-			// more widely supported
-			return req.get();
-		} )
-		// Inspect response to find API location header
-		.then( autodiscovery.locateAPIRootHeader )
-		.then( ( apiRootURL ) => {
-			// Set the function-scope variable that will be used to instantiate
-			// the bound WPAPI instance,
-			endpoint = apiRootURL;
-
-			// then GET the API root JSON object
-			return WPAPI.site( apiRootURL ).root().get();
-		} )
-		.then( ( apiRootJSON ) => {
-			// Instantiate & bootstrap with the discovered methods
-			return new WPAPI( {
-				endpoint: endpoint,
-				routes: apiRootJSON.routes,
-			} );
-		} )
-		.catch( ( err ) => {
-			/* eslint-disable no-console */
-			console.error( err );
-			if ( endpoint ) {
-				console.warn( 'Endpoint detected, proceeding despite error...' );
-				console.warn( 'Binding to ' + endpoint + ' and assuming default routes' );
-				return new WPAPI.site( endpoint );
-			}
-			throw new Error( 'Autodiscovery failed' );
+	// Use WPAPI.site to make a request using the defined transport
+	const req = WPAPI.site( url ).root().param( 'rest_route', '/' );
+	return req.get().then( ( apiRootJSON ) => {
+		const routes = apiRootJSON.routes;
+		return new WPAPI( {
+			// Derive the endpoint from the self link for the / root
+			endpoint: routes['/']._links.self,
+			// Bootstrap returned WPAPI instance with the discovered routes
+			routes: routes,
 		} );
+	} );
 };
 
 module.exports = WPAPI;
